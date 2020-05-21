a383acdc1e5b4839be86840d4e7329b8b2576ace
Kirill Zaborsky <avto.oil@gmail.com>
2020-05-21 13:37:18 +0700

    Добавлен функционал приложения

--- HW - traffic light/ViewController.swift
+++ HW - traffic light/ViewController.swift
@@ -18,10 +18,46 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        redSignal.layer.cornerRadius = 50
+        yellowSignal.layer.cornerRadius = 50
+        greenSignail.layer.cornerRadius = 50
+        
+        redSignal.alpha = 0.3
+        yellowSignal.alpha = 0.3
+        greenSignail.alpha = 0.3
+        
+        
+        startButton.layer.cornerRadius = 10
     }
-
+    
+    var lightStatus = 0
+    
     @IBAction func startButtonPress() {
+        startButton.setTitle("NEXT", for: .normal)
+        
+        switch lightStatus {
+            
+        case 0:
+            redSignal.alpha = 1
+            yellowSignal.alpha = 0.3
+            greenSignail.alpha = 0.3
+            lightStatus += 1
+            
+        case 1:
+            redSignal.alpha = 0.3
+            yellowSignal.alpha = 1
+            lightStatus += 1
+            
+        case 2:
+            redSignal.alpha = 0.3
+            yellowSignal.alpha = 0.3
+            greenSignail.alpha = 1
+            lightStatus = 0
+            
+        default:
+            break
+        }
+        
     }
     
 }
